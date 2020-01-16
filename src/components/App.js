2ab0b9b7bcbb20538f30c3d05c3b899448adfd8f
programmingArrow <nishant.kashyap2104@gmail.com>
2020-01-16 23:52:30 +0530

    added languages in state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+    state = {
+        languages : [
+            {
+                name: 'PHP',
+                vote: 0
+            },
+            {
+                name: 'Python',
+                vote: 0
+            }
+        ]
+    }
+
     render() {
         return (
             <div>
-                
+                { this.state.languages.map(language => <li>{ language.name }</li>) }
             </div>
         )
     }
